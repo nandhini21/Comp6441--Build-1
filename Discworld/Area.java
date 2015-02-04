4ad605cf7de401a3bb1a0da7e0cee6e699554b50
kumaran1024 <kumaranayyappan10@gmail.com>
2015-02-04 08:44:31 +0530

    king

---
+++ Discworld/Area.java
@@ -0,0 +1,19 @@
+package Discworld;
+
+public class Area 
+{
+
+	String areaName;
+	String areaNumber;
+	String areaPower;
+	int buildCost;
+	
+	String[] minionsPresent = new String[4];
+	String buildingPresent;
+	int trollsPresent;
+	int demonsPresent;
+	
+	boolean troubleMarker = false;
+	
+	
+}
